b1bccd6bbcb36a2f0ed8f348a5f27c611a1f89ea
Dhanishetty-Ganesh <varmaganesh224@gmail.com>
2024-08-01 15:13:07 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import "./App.css"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="agriculture-questions-container">
+      <h1 className="agriculture-questions-title">Agriculture Questions</h1>
+      <ul className="agriculture-questions-list">
+        <li className="agriculture-question">What is the role of water in Plants?</li>
+        <li className="agriculture-question">What are the two sources of water for plants?</li>
+        <li className="agriculture-question">Explain two sources of irrigation water with the help of flowchart?</li>
+        <li className="agriculture-question">List out the various methods of irrigation systems with the help of flow chart?</li>
+        <li className="agriculture-question">Write about flood irrigation method?</li>
+        <li className="agriculture-question">Write about border irrigation method</li>
+        <li className="agriculture-question">write about drip irrigation method with its components</li>
+        <li className="agriculture-question">write about sprinkler irrigation system</li>
+        <li className="agriculture-question">write about furrow irrigation system</li>
+      </ul>
     </div>
   );
 }

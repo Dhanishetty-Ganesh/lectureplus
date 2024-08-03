3c106f5a599563890301d1b592d007c5c66cfdb1
Dhanishetty-Ganesh <varmaganesh224@gmail.com>
2024-08-03 14:04:32 +0530

    second commits

--- src/App.js
+++ src/App.js
@@ -4,17 +4,20 @@ import "./App.css"
 function App() {
   return (
     <div className="agriculture-questions-container">
-      <h1 className="agriculture-questions-title">Agriculture Questions</h1>
       <ul className="agriculture-questions-list">
-        <li className="agriculture-question">What is the role of water in Plants?</li>
-        <li className="agriculture-question">What are the two sources of water for plants?</li>
-        <li className="agriculture-question">Explain two sources of irrigation water with the help of flowchart?</li>
-        <li className="agriculture-question">List out the various methods of irrigation systems with the help of flow chart?</li>
-        <li className="agriculture-question">Write about flood irrigation method?</li>
-        <li className="agriculture-question">Write about border irrigation method</li>
-        <li className="agriculture-question">write about drip irrigation method with its components</li>
-        <li className="agriculture-question">write about sprinkler irrigation system</li>
-        <li className="agriculture-question">write about furrow irrigation system</li>
+      <li className="agriculture-question bit-name">I) Very short Answer questions         (4 x 1 = 4M)</li>
+        <li className="agriculture-question">1.Define Irrigation?</li>
+        <li className="agriculture-question">2.List out the two sources of water?</li>
+        <li className="agriculture-question">3.List out the two sources of irrigation water?</li>
+        <li className="agriculture-question">4.List out the methods of irrigation?</li>
+        <li className="agriculture-question bit-name">II) Short Answer questions         (4 x 2 = 8M)</li>
+        <li className="agriculture-question">1.Explain about two sources of water with flow chart?</li>
+        <li className="agriculture-question">2.Explain about two sources of irrigation water along with flow chart?</li>
+        <li className="agriculture-question">3.Write about flood irrigation system?</li>
+        <li className="agriculture-question">4.What is the role of water in plants?</li>
+        <li className="agriculture-question bit-name">III) Long Answer questions         (2 x 4 = 8M)</li>
+        <li className="agriculture-question">1.Write about sprinkler irrigation system?</li>
+        <li className="agriculture-question">2.Explain about Drip Irrigation system along with its components?</li>        
       </ul>
     </div>
   );
